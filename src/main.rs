d17674431f2ab9462eacadb9ec54d1d6f374d048
Tom Cornell <tom.cornell@gmail.com>
2017-01-30 15:00:51 -0500

    Add command line argument parsing
    Stdin is now the text to match. The regex is the cmdline arg.
    There is also an option to read the text from a file.

--- src/main.rs
+++ src/main.rs
@@ -6,31 +6,94 @@
  * representing the remaining unparsed string.
  */
 
+extern crate getopts;
+
+use getopts::Options;
+use std::env;
+use std::process;
+
 use std::io;
 use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
 
 mod reterm;
 mod reparse;
 mod retrans;
+mod reinterp;
 
 use reparse::*;
 use retrans::*;
 
-fn main() {
-    // Get stdin into a string
-    let stdin = io::stdin();
-    let mut s = String::new();
-    stdin.lock().read_to_string(&mut s).unwrap();
-    println!("{}", s);
+struct AppConfig {
+    in_file: Option<String>,
+    expr: String
+}
 
-    let t = parse(&s);
-    println!("{}", t);
+impl AppConfig {
+    fn new() -> AppConfig {
+        AppConfig { 
+            in_file: None,
+            expr: "".to_string()
+        }
+    }
+}
+
+fn configure() -> AppConfig {
+    let mut cfg: AppConfig = AppConfig::new();
+    let args: Vec<String> = env::args().collect();
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "print this message and exit");
+    opts.optopt("f", "file", "match text from file", "NAME");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
+    if matches.opt_present("h") {
+        print_usage(&args[0], &opts);
+    }
+    cfg.in_file = matches.opt_str("f");
+    if matches.free.is_empty() {
+        print_usage(&args[0], &opts);
+    } else {
+        cfg.expr = matches.free[0].clone();
+    }
+
+    cfg
+}
+
+fn print_usage(program: &str, opts: &Options) {
+    let brief = format!("Usage: {} [options] REGEX", program);
+    print!("{}", opts.usage(&brief));
+    process::exit(1);
+}
+
+fn main() {
+    let cfg = configure();
+
+    let mut text = String::new();
+    match cfg.in_file {
+        None => {
+            let stdin = io::stdin();
+            stdin.lock().read_to_string(&mut text).unwrap();
+        },
+        Some(fname) => {
+            let fpath = Path::new(&fname);
+            let mut f = File::open(fpath).unwrap();
+            f.read_to_string(&mut text).unwrap();
+        }
+    }
+    println!("{}", text);
+
+
+    let tree = parse(&cfg.expr);
+    println!("{}", tree);
 
     let mut translator = RegexTranslator::new();
 
     // Maybe the following doesn't have to be a borrow?
     // At this point we should be done with t...
-    translator.compile(&t);
+    translator.compile(&tree);
     translator.print_prog();
 }
 
